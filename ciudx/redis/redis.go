c58ae90db116cdc5fbcf7d626e9289b4920b2fb4
Abhishek Upperwal <a.upperwal@gmail.com>
2021-02-27 20:11:48 +0530

    redis log

--- ciudx/redis/redis.go
+++ ciudx/redis/redis.go
@@ -3,6 +3,11 @@ package redis
 import (
 	utils "github.com/dataspace-mobility/rs-iudx/ciudx/utils"
 	redis "github.com/go-redis/redis/v8"
+	logging "github.com/ipfs/go-log"
+)
+
+var (
+	log = logging.Logger("redis")
 )
 
 // Connection holds the redis connection.
@@ -16,6 +21,8 @@ func NewRedisConnection() *Connection {
 	port := utils.Getenv("REDIS_PORT", "6379")
 	username := utils.Getenv("REDIS_USERNAME", "")
 	password := utils.Getenv("REDIS_PASSWORD", "")
+
+	log.Info("Redis connecting on ", host, ":", port)
 
 	return &Connection{
 		Client: redis.NewClient(&redis.Options{
